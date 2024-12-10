2e988f0ebb0d69200116938200820315a75fba96
Olin <olin.goog@gmail.com>
2024-12-10 17:10:12 -0500

    control merged with predictor

--- control.py
+++ control.py
@@ -1,18 +1,27 @@
 from task1 import task
 from teleop import game
 from pyniryo2 import *
-from brain import predictor
+import Predictor
 
 import threading
 import time
 
+import brain_data
+
 event = threading.Event()
+
+model_path = "beststatedict"
+data_path = "data"
 
 
 def thread():
+    model = predictor.Predictor(model_path)
+    sub_feature, __ = brain_data.read_subject_csv_binary(os.path.join(data_path, "sub_1.csv"), num_chunk_this_window_size=1488)
+    index = 0
     while True:
         time.sleep(.6)
-        prediction = True
+        index += 1
+        prediction = model.predict(sub_feature(index))
         if prediction:
             event.set()
         else:
